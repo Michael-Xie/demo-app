5447450049f84bec728754232a09cb3df3391c62
Michael-Xie <michael@ifiveo.com>
2022-02-07 09:39:59 -0500

    Show API key and remove unused assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  console.log(process.env.REACT_APP_NASA_API_KEY);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello world</p>
+      <p>API key: {process.env.REACT_APP_NOT_SECRET_CODE}</p>
     </div>
   );
 }
